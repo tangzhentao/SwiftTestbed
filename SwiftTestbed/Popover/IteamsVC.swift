8367296c954b9bae8dfc2812ee36ef7813119215
tang <itangzhentao@126.com>
2020-12-09 20:14:13 +0800

    popover

--- SwiftTestbed/Popover/IteamsVC.swift
+++ SwiftTestbed/Popover/IteamsVC.swift
@@ -6,9 +6,11 @@
 //
 
 import Foundation
+import UIKit
 
 class IteamsVC: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor.blue
     }
 }
--- SwiftTestbed/Popover/PopoverVC.swift
+++ SwiftTestbed/Popover/PopoverVC.swift
@@ -16,7 +16,7 @@ class PopoverVC: BaseViewController {
     }()
     
     var btn: UIButton!
-    let items = IteamsVC()
+    var items: IteamsVC!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,17 +30,7 @@ class PopoverVC: BaseViewController {
     func addBarButtonItems() {
         navigationItem.rightBarButtonItem = addItem
     }
-    func addButton() {
-        btn = UIButton(frame: CGRect(x: 60, y: 100, width: 100, height: 60))
-        btn.addTarget(self, action: #selector(popButton), for: UIControl.Event.touchUpInside)
-        btn.backgroundColor = UIColor.green
-        view.addSubview(btn)
-        
-        items.preferredContentSize = CGSize(width: 80, height: 160)
-        items.modalPresentationStyle = .popover
-        
-        
-    }
+    
     func addPopover() {
         let items = IteamsVC()
         items.preferredContentSize = CGSize(width: 80, height: 160)
@@ -60,19 +50,38 @@ class PopoverVC: BaseViewController {
         addPopover()
     }
     
+    func addButton() {
+        btn = UIButton(frame: CGRect(x: 60, y: 100, width: 100, height: 60))
+        btn.addTarget(self, action: #selector(popButton), for: UIControl.Event.touchUpInside)
+        btn.backgroundColor = UIColor.green
+        view.addSubview(btn)
+        
+        // 创建vc
+        items = IteamsVC()
+        // 设置弹出框的大小
+        items.preferredContentSize = CGSize(width: 80, height: 160)
+        // 设置显示的样式为弹出框
+        items.modalPresentationStyle = .popover
+    }
+    
     @objc func popButton() {
         print("\(type(of: self)).\(#function)")
         
+        // 获取vc的展示vc 并 设置该展示vc
         let pop = items.popoverPresentationController
         pop?.delegate = self
         pop?.sourceView = btn
-                
+        pop?.sourceRect = CGRect(x: 90, y: 50, width: 10, height: 10)
+        pop?.backgroundColor = UIColor.orange
+        print(pop?.popoverLayoutMargins as Any)
+        // 显示vc
         self.present(items, animated: true, completion: nil)
     }
     
     
 }
 
+// 提供该方法，则不能呈现弹出框样式
 extension PopoverVC: UIPopoverPresentationControllerDelegate {
     func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
         return .none
